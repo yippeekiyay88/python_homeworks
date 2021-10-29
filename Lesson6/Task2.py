ddffaab513fdd2c9c3acae51e129437c2cb17494
yippeekiyay88 <sudar28@mail.ru>
2021-10-29 10:22:06 +0300

    added 6 homework tasks

---
+++ Lesson6/Task2.py
@@ -0,0 +1,23 @@
+# 2. Реализовать класс Road (дорога), в котором определить атрибуты: length (длина), width (ширина).
+# Значения данных атрибутов должны передаваться при создании экземпляра класса.
+# Атрибуты сделать защищенными. Определить метод расчета массы асфальта, необходимого для покрытия
+# всего дорожного полотна. Использовать формулу: длина * ширина * масса асфальта для покрытия
+# одного кв метра дороги асфальтом, толщиной в 1 см * чи сло см толщины полотна. Проверить работу метода.
+# Например: 20м * 5000м * 25кг * 5см = 12500 т
+
+class Road:
+
+    def __init__(self, length, width):
+        self._length = length
+        self._width = width
+        self.weight = 25
+        self.height = 5
+
+    def asphalt_mass(self):
+        asphalt_mass = self._length * self._width * self.weight * self.height / 1000
+        print(f'For cover needs {round(asphalt_mass)}t mass of the asphalt')
+
+
+r = Road(5000, 20)
+r.asphalt_mass()
+
